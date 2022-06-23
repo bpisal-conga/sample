708b2049f4d484fbfc38c9333f208a7172b407b7
Bhagyashri Pisal <bpisal@conga.com>
2022-06-23 12:37:52 +0530

    JIRA 9876- added another file --DemoTest

---
+++ src/p1/DemoTest.java
@@ -0,0 +1,14 @@
+package p1;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println(" Welcome Conga ..");
+		
+		System.out.println(" Demo test purpose...!!");
+	}
+
+}
